babdcf7febf032096ed7a2858ee4b27f28cde250
Tom <tom-dev@abracode.com>
2025-10-30 23:05:45 -0700

    Update codesigning support files

--- codesign_app.sh
+++ codesign_app.sh
@@ -8,9 +8,14 @@ if test -z "$app_to_sign"; then
 	exit 1
 fi
 
+# full path
+app_to_sign=$(/bin/realpath "$app_to_sign")
+
 echo ""
 echo "errors from install_name_tool about missing LC_RPATH are ignorable (all is as expected)"
+echo "-------------------------"
 /usr/bin/install_name_tool -delete_rpath "@loader_path/../../../Frameworks" "$app_to_sign/Contents/MacOS/OMCApplet"
+echo "-------------------------"
 echo ""
 
 app_id=$(/usr/bin/defaults read "$app_to_sign/Contents/Info.plist" CFBundleIdentifier)
@@ -19,4 +24,12 @@ if test "$?" != "0"; then
 	exit 1
 fi
 
+echo "/usr/bin/codesign --deep --verbose --force --options runtime --entitlements $self_dir/OMCApplet.entitlements --timestamp --identifier $app_id --sign T9NM2ZLDTY $app_to_sign"
 /usr/bin/codesign --deep --verbose --force --options runtime --entitlements "$self_dir/OMCApplet.entitlements" --timestamp --identifier "$app_id" --sign "T9NM2ZLDTY" "$app_to_sign"
+
+echo ""
+echo "Verifying codesigned app:"
+echo "-------------------------"
+codesign -dv --verbose=4 "$app_to_sign"
+echo "-------------------------"
+
